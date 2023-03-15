94fb4b4f0b31454cdc2a328cb58005847631da13
Samuel <samueldesta323@gmail.com>
2023-03-15 23:53:41 +0300

    added interview question rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Rotate 2d Matrix"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix):
+    """rotates <matrix> 90deg clockwise"""
+    transpose(matrix)
+    for li in matrix:
+        i, j = 0, len(li) - 1
+        while i < j:
+            li[i], li[j] = li[j], li[i]
+            i += 1
+            j -= 1
+
+
+def transpose(matrix: List[List[int]]):
+    """returns the transpose of <matrix>"""
+    i = 0
+    while i < len(matrix):
+        j = i + 1
+        while j < len(matrix[i]):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            j += 1
+        i += 1
