0029e7e598a62e9d5095153bf9e4cd9e87209128
Samuel <samueldesta323@gmail.com>
2023-03-30 23:10:09 +0300

    added interview question 9

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Island perimeter solver"""
+
+def island_perimeter(grid):
+    """solves Island perimeter"""
+    (row, column) = (len(grid), len(grid[0]))
+    perimeter = 0
+    for i in range(row):
+        for j in range(column):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == row - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == column - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,2 @@
+# island perimeter
+<hr/>
